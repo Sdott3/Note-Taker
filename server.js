03fb8938c195355ae4bfbe57cca982fa0dfe0e8a
sdott3 <sdotthomas347@gmail.com>
2022-08-05 23:40:09 -0400

    added parse for data, JSON, and middleware for public files

--- server.js
+++ server.js
@@ -7,3 +7,15 @@ const PORT = process.env.PORT || 3001;
 
 // start the server
 const app = express();
+
+// parse incoming data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON
+app.use(express.json());
+// Pparse middleware for public files
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
+  
